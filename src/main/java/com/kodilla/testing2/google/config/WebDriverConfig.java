23da3c31394cca2b8f05c09eed12ef6c75ad7f34
aeronaut <aeronaut@MacBook-Pro-aeronaut.local>
2017-12-07 15:37:19 +0100

    first commit

---
+++ src/main/java/com/kodilla/testing2/google/config/WebDriverConfig.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing2.google.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverConfig {
+
+    public final static String CHROME = "CHROME_DRIVER";
+
+    public static WebDriver getDriver(final String driver) {
+        System.setProperty("webdriver.chrome.driver", "/Users/aeronaut/Documents/Java/Kodilla/kodilla-DevTools/Selenium-drivers/chromedriver");
+        if (driver.equals(CHROME)) {
+            return new ChromeDriver();
+        } else {
+            return null;
+        }
+    }
+    
+}
